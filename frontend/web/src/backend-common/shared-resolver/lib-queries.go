484bb68642a13a4e187642d6b4a0a422ade281f9
cluctinotroma <cluctinotroma@users.noreply.github.com>
2025-11-09 14:50:07 +0000

    Add dynamic-common.sass, lib-queries.go

---
+++ frontend/web/src/backend-common/shared-resolver/lib-queries.go
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/lottocrypt_z/frontend/web/src/backend-common/shared-resolver/lib-queries.go
+// Generated at: 2025-11-27T15:10:54.771Z
+
+
